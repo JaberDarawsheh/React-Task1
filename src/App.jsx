fab2454f7732d7ba1854429cae08723959edeb2f
JaberDarawsheh <jaberdarawsheh@gmail.com>
2024-02-16 11:32:27 +0200

    react-task1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let name="Jaber Darawsheh";
+  let age =21;
+  return (
+    <>
+      <h2>Hello <strong>{name}</strong></h2>
+      <br />
+      <h2> My age is <strong>{age}</strong></h2>
+    </>  
+  )
+}
+
+export default App
